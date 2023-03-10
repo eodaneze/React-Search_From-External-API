c25c8e7c96a717636303798e279daf1c570936fe
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-03-10 06:57:19 -0800

    creating a state the we track the value of the input field

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 const SearchBox = () => {
+  const [input, setInput] = useState("");
+
   return (
     <div>
       <div className="search">
-        <AiOutlineSearch size={20} color={"blue"}/>
-        <input type="search" placeholder="Type to search..." />
+        <AiOutlineSearch size={20} color={"blue"} />
+        <input
+          type="search"
+          placeholder="Type to search..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
       </div>
+      <p>{input}</p>
     </div>
   );
 };
